886cb41c46b102876763abe516a1bb696cd76bd2
anderson <zapataander18@gmail.com>
2026-03-07 17:00:34 -0500

    feat: add AWS key pair data source and reference it in EC2 instance configuration

--- iac/day_06/ec2.tf
+++ iac/day_06/ec2.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "public_instance" {
   ami           = "ami-02dfbd4ff395f2a1b"
   instance_type = "t3.micro"
   subnet_id     = aws_subnet.public_subnet.id
+  key_name = data.aws_key_pair.key.key_name
   tags = {
     Name = "HelloWorld"
   }
